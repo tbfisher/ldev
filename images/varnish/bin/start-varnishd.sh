b8bb9b59a5a4f7e298b64f799287f83a605edba7
Brian Fisher <tbfisher@gmail.com>
2019-06-12 06:53:26 -0600

    make varnish vcl configurable via env variables
    # Conflicts:
    #	docker-compose.bitswreck.yml
    #	images/varnish/conf-dev/default.vcl

--- images/varnish/bin/start-varnishd.sh
+++ images/varnish/bin/start-varnishd.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+set -e
+
+# Write env vars to config.
+
+# shellcheck disable=2039
+vars=(
+    VARNISH_VCL_DEFAULT_BACKEND_HOST
+    VARNISH_VCL_DEFAULT_BACKEND_PORT
+    VARNISH_VCL_DEFAULT_ACL_PURGE_BAN
+)
+# shellcheck disable=2039
+for var in "${vars[@]}"; do
+    eval "val=\$$var"
+    # shellcheck disable=2154
+    sed -i 's@%'"$var"'%@'"$val"'@' /etc/varnish/default.vcl
+done
+
 exec varnishd \
   -j unix,user=varnishd \
   -F \
